16eb2953861406e225cd92638eff8837cbc96758
KhanhNguyen <ankhanh1566@gmail.com>
2021-10-26 19:56:01 +0700

    U

---
+++ FIllBlank/main.py
@@ -0,0 +1,90 @@
+# import tensorflow as tf
+
+# label = "Xin chào!"
+# preprocess = "xin_chao_"
+# output = "xin_chào_"
+# processOutput = "Xin chào!"
+
+label = "Xin chào!"
+
+
+preprocess = label.lower()
+preprocess = label.lower()
+'''
+à
+á
+ă
+â
+ã
+
+Ă
+ă
+Â
+â
+B
+b
+C
+c
+D
+d
+Đ
+đ
+E
+e
+Ê
+ê
+F
+f
+G
+g
+H
+h
+I
+i
+J
+j
+K
+k
+L
+l
+M
+m
+N
+n
+O
+o
+Ô
+ô
+Ơ
+ơ
+P
+p
+Q
+q
+R
+r
+S
+s
+T
+t
+U
+u
+Ư
+ư
+V
+v
+W
+w
+X
+x
+Y
+y
+Z
+z
+'''
+preprocess = "xin_chao_"
+
+
+output = "xin_chào_"
+
+processOutput = "Xin chào!"
