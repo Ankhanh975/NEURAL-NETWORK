ce0e89bff00b958b4412a58600ad4fcb469a52fb
Gold_Apple_Vn <ankhanh1566@gmail.com>
2021-10-27 14:44:58 +0700

    U

--- FIllBlank/model.py
+++ FIllBlank/model.py
@@ -1,12 +1,49 @@
 import tensorflow as tf
-import numpy as np
-import pandas as pd
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.callbacks import EarlyStopping
-from keras.callbacks import ModelCheckpoint
-from tensorflow.keras.models import load_model
-import time
-import os
-import json
+from tensorflow.keras.models import Sequential
+# Use CuDNNLSTM if GPU available!
+from tensorflow.keras.layers import Dense, Dropout, LSTM, Embedding
+from tensorflow.python.keras.layers.recurrent import SimpleRNN
 
+X_train = y_train = []
+X_test = y_test = []
+with open("from/aticle3.txt", "r", encoding="utf-8") as r:
+    x = r.read()
+    for i in x:
+        X_train.append(i)
+
+with open("to/aticle3.txt", "r", encoding="utf-8") as r:
+    x = r.read()
+    for i in x:
+        y_train.append(i)
+        
+with open("from/aticle3.txt", "r", encoding="utf-8") as r:
+    x = r.read()
+    for i in x:
+        X_test.append(i)
+
+with open("to/aticle3.txt", "r", encoding="utf-8") as r:
+    x = r.read()
+    for i in x:
+        y_test.append(i)
+
+
+model = Sequential()
+
+# Input layer
+model.add(Embedding(1000, 64, input_length=10))
+
+model.add(SimpleRNN(128, activation='relu', return_sequences=True))
+model.add(Dropout(0.1))
+model.add(SimpleRNN(128, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(10, activation='softmax'))
+
+
+opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)    # Optimizer
+model.compile(loss='sparse_categorical_crossentropy',  # Loss
+              optimizer=opt,
+              metrics=['accuracy'])
+              
+model.fit(X_train, y_train, epochs=3, validation_data=(X_test, y_test))
